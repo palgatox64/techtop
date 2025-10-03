1216abbce1bf245c88a4a13cd32251ac9f900241
palgatox64 <se.navarrete@hotmail.com>
2025-10-03 00:43:21 -0300

    feat: Añade página de contacto con formulario y estilos

--- store/views.py
+++ store/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
--- techtop_project/urls.py
+++ techtop_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from store.views import home # Asegúrate de importar la vista
+from store.views import home, contacto # Asegúrate de importar la vista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'), # Añade esta línea para la página de inicio
+    path('contacto/', contacto, name='contacto'), # Añade esta línea para la página de contacto
 ]
