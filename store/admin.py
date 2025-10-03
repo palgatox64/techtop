c39ff78079c286bd72e646551686da175dd8c2dd
palgatox64 <se.navarrete@hotmail.com>
2025-10-02 23:12:14 -0300

    feat: Registra los modelos en el panel de administración de Django

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Categoria, Marca, Producto, Cliente, Direccion, Pedido, DetallePedido
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Marca)
+admin.site.register(Producto)
+admin.site.register(Cliente)
+admin.site.register(Direccion)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
