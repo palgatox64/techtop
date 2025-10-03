5696b8a9ece13c631e3433091b32f4168810cb13
Javier Granja <ja.granja@duocuc.cl>
2025-10-03 01:01:48 -0300

    creacion de page home

--- static/js/main.js
+++ static/js/main.js
@@ -22,4 +22,25 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         }
     });
+
+    document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true, 
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000, 
+            disableOnInteraction: false,
+        },
+    });
+
+    
+});
 });
