08e0d7f6b6490ae60a8fa5cece13da1d45f316a0
Javier Granja <ja.granja@duocuc.cl>
2025-10-06 20:27:02 -0300

    responsive

--- static/js/main.js
+++ static/js/main.js
@@ -1,58 +1,96 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-
-    console.log("Intentando inicializar Swiper...");
-
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal',
         loop: true,
-      
         pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+            el: '.swiper-pagination',
+            clickable: true,
         },
-      
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-
         autoplay: {
             delay: 5000,
             disableOnInteraction: false,
         },
     });
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    
     const userMenuButton = document.getElementById('user-menu-button');
     const userMenuDropdown = document.getElementById('user-menu-dropdown');
-    
     const userMenuButtonLoggedIn = document.getElementById('user-menu-button-loggedin');
     const userMenuDropdownLoggedIn = document.getElementById('user-menu-dropdown-loggedin');
 
     if (userMenuButton) {
         userMenuButton.addEventListener('click', function(event) {
-            event.stopPropagation(); 
+            event.stopPropagation();
             userMenuDropdown.classList.toggle('show');
         });
     }
-
     if (userMenuButtonLoggedIn) {
         userMenuButtonLoggedIn.addEventListener('click', function(event) {
-            event.stopPropagation(); 
+            event.stopPropagation();
             userMenuDropdownLoggedIn.classList.toggle('show');
         });
     }
 
+    const dropdownItems = document.querySelectorAll('.nav-item-dropdown');
+    dropdownItems.forEach(item => {
+        const link = item.querySelector('.nav-link');
+        link.addEventListener('click', function(event) {
+            if (window.innerWidth <= 768) {
+                if (!item.classList.contains('active')) {
+                    event.preventDefault(); 
+                    dropdownItems.forEach(openItem => {
+                        if (openItem !== item) {
+                            openItem.classList.remove('active');
+                        }
+                    });
+                    
+                    item.classList.add('active');
+                }
+            }
+        });
+    });
+
+  
     window.addEventListener('click', function(event) {
-        if (userMenuDropdown && userMenuDropdown.classList.contains('show')) {
+        if (userMenuDropdown && userMenuDropdown.classList.contains('show') && !userMenuButton.contains(event.target)) {
             userMenuDropdown.classList.remove('show');
         }
-        if (userMenuDropdownLoggedIn && userMenuDropdownLoggedIn.classList.contains('show')) {
+        if (userMenuDropdownLoggedIn && userMenuDropdownLoggedIn.classList.contains('show') && !userMenuButtonLoggedIn.contains(event.target)) {
             userMenuDropdownLoggedIn.classList.remove('show');
         }
+        dropdownItems.forEach(item => {
+            if (!item.contains(event.target)) {
+                item.classList.remove('active');
+            }
+        });
     });
+
+    const openBtn = document.getElementById('open-filters-btn');
+    const closeBtn = document.getElementById('close-filters-btn');
+    const filterSidebar = document.getElementById('filter-sidebar');
+    const backdrop = document.getElementById('filter-backdrop');
+
+    if (openBtn) {
+        openBtn.addEventListener('click', function() {
+            filterSidebar.classList.add('is-open');
+            backdrop.classList.add('is-active');
+        });
+    }
+
+    if (closeBtn) {
+        closeBtn.addEventListener('click', function() {
+            filterSidebar.classList.remove('is-open');
+            backdrop.classList.remove('is-active');
+        });
+    }
+
+    if (backdrop) {
+        backdrop.addEventListener('click', function() {
+            filterSidebar.classList.remove('is-open');
+            backdrop.classList.remove('is-active');
+        });
+    }
 });
